1a45253ecb468e2999299b233473591a546c8dd8
32r143 <60690547+32r143@users.noreply.github.com>
2020-05-05 10:47:18 +0530

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,11 +1,20 @@
+import sys
 def collatz(number):
-    if number %2 ==0:
-        result = number //2
-    else :
-        result = 3*number+1
+    if number %2 == 0:
+        result = number//2
+    else:
+        result = 3*number + 1
     print(result)
     return result
-random = int(input('Enter an integer'))
-while random != 1:
-    random = collatz(random)
-    
+
+print("Enter an integer")
+while True:
+    try:
+        userinput = int(input())
+    except ValueError:
+        print('Entered value is not an integer value')
+        continue
+    while userinput != 1:
+        userinput = collatz(userinput)
+    if userinput == 1:
+        sys.exit()
